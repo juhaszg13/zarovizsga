c3f757b9b1405e536223f95d65c4a1d7cf4403fe
juhaszg13 <juhaszg13@gmail.com>
2021-03-17 20:51:52 +0100

    DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -4,24 +4,21 @@ public class DigitsCounter {
 
     public int getCountOfDigits(String digit) {
 
-        if (digit.equals("")||"null".equals(digit)) {
+        if (digit==null||digit.equals("")) {
             return 0;
         }
+
         String digits = "0123456789";
         String[] tmp = digit.split("");
         int[] counter = new int[10];
         int sum = 0;
 
         for (String i : tmp) {
-            if (digits.contains(i)) {
-                counter[Integer.parseInt(i)]++;
-            }
-        }
-        for (int i : counter) {
-            if (i > 0) {
+            if (digits.contains(i)&&(counter[Integer.parseInt(i)]==0)) {
                 sum++;
             }
         }
         return sum;
     }
 }
+
